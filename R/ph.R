169b41b0a45922928709c7243d1a45cfb1d4b0c3
pvictor <perrier.victor@gmail.com>
2021-07-14 19:32:37 +0200

    added ph()

---
+++ R/ph.R
@@ -0,0 +1,37 @@
+
+#' @title Phosphor Icons
+#'
+#' @description Create a `<svg></svg>` tag to render an icon.
+#'
+#' @param name Name of the icon to use.
+#' @param type Type of icon.
+#' @param fill Fill color for the icon.
+#' @param height,width Height and width in valid CSS unit.
+#' @param ... Attributes passed to the SVG tag.
+#'
+#' @return An SVG tag.
+#' @export
+#'
+#' @importFrom htmltools tagAppendAttributes validateCssUnit browsable
+ph <- function(name,
+               type = c("light", "regular", "thin", "bold", "fill"),
+               fill = "#333",
+               height = "1.33em",
+               width = NULL,
+               ...) {
+  type <- match.arg(type)
+  # name <- check_icon(name)
+  if (!is.null(height))
+    height <- validateCssUnit(height)
+  if (!is.null(width))
+    width <- validateCssUnit(width)
+  svg <- ph_icons[[paste(name, type, sep = "-")]]
+  svg <- tagAppendAttributes(
+    tag = svg,
+    height = height,
+    width = width,
+    fill = fill,
+    ...
+  )
+  browsable(svg)
+}
