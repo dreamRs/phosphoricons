bcf38f4ae7b8f8ae7c91e91c635ab2733c7f022f
pvictor <perrier.victor@gmail.com>
2021-07-15 12:51:19 +0200

    added search_icon()

---
+++ R/search.R
@@ -0,0 +1,38 @@
+
+#' Search Icons
+#'
+#' @param pattern Pattern to search for in icon's name.
+#' @param view_results View icons found in viewer.
+#'
+#' @return a character vector invisibly.
+#' @export
+#' 
+#' @importFrom htmltools browsable tags
+#'
+#' @examples
+#' search_icon("calendar")
+#' search_icon("text")
+search_icon <- function(pattern, view_results = interactive()) {
+  search_name <- grep(pattern = pattern, x = phosphoricons_names, value = TRUE)
+  if (length(search_name) < 1) {
+    warning("No icon found...")
+    return(invisible(character(0)))
+  }
+  view_icons <- browsable(tags$div(
+    lapply(
+      X = search_name, 
+      FUN = function(name) {
+        tags$div(
+          style = "display: inline-block; text-align: center; margin: 5px;",
+          ph(name, height = 64),
+          tags$br(),
+          name
+        )
+      }
+    )
+  ))
+  if (isTRUE(view_results))
+    print(view_icons)
+  return(invisible(search_name))
+}
+
