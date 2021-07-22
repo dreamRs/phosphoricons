33edfba3d64cfeb24ba76b31613319fd73f6f340
pvictor <perrier.victor@gmail.com>
2021-07-22 16:39:37 +0200

    ph: added vertical align + currentColor

--- R/ph.R
+++ R/ph.R
@@ -1,26 +1,28 @@
 
-#' @title Phosphor Icons
+#' @title Phosphor Icon
 #'
 #' @description Create a `<svg></svg>` tag to render an icon.
 #'
 #' @param name Name of the icon to use.
 #' @param type Type of icon (from thinnest to thickest): `thin`, `light` (default), `regular`, `bold` or `fill`.
-#' @param fill Fill color for the icon.
+#' @param fill Fill color for the icon, default is `currentColor` which should match the color of the text where the icon is used.
 #' @param height,width Height and width in valid CSS unit.
+#' @param vertical_align Vertical alignment for the icon, this depend on the size of the icon.
 #' @param title Add a `<title>` tag to provides an accessible, short-text description of the icon. Use `NULL` for no title.
 #' @param ... Attributes passed to the SVG tag.
 #'
 #' @return An SVG tag.
 #' @export
 #'
-#' @importFrom htmltools tagAppendAttributes validateCssUnit browsable tagAppendChild tags
+#' @importFrom htmltools tagAppendAttributes validateCssUnit browsable tagAppendChild tags css
 #' 
 #' @example examples/ph.R
 ph <- function(name,
                type = c("light", "regular", "thin", "bold", "fill"),
-               fill = "#333",
+               fill = "currentColor",
                height = "1.33em",
                width = NULL,
+               vertical_align = "-0.25em",
                title = name,
                ...) {
   type <- match.arg(type)
@@ -35,6 +37,7 @@ ph <- function(name,
     height = height,
     width = width,
     fill = fill,
+    style = css(vertical_align = vertical_align),
     ...
   )
   if (!is.null(title)) {
--- examples/ph-shiny.R
+++ examples/ph-shiny.R
@@ -5,12 +5,16 @@ ui <- navbarPage(
   title = "My app",
   tabPanel(
     title = "Page",
-    icon = ph("house", style = "vertical-align: -0.125em;"),
-    tags$h1(ph("package", style = "vertical-align: -0.125em;"), "This is a title"),
+    icon = ph("house"),
+    tags$h1(ph("package"), "This is a title"),
     tags$p(
       "An icon", ph("cat"), "inside a paragraph"
     ),
-    actionButton("btn1", tagList(ph("play", style = "vertical-align: -0.125em;"), "a button"))
+    wellPanel(
+      style = "background: #151515; color: #FFF;",
+      "This icon should be white:", ph("crown", type = "bold")
+    ),
+    actionButton("btn1", tagList(ph("play"), "a button"))
   )
 )
 
